e9178bbc3be84ae0421b4962e7c60274bdae2841
adekunleo1 <adekunle.ogunleye1@gmail.com>
2022-12-18 15:21:14 +0100

    Configuration MGT

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,7 +1,10 @@
-# Create a file
+# Creates a file
+
 file { '/tmp/school':
-    mode    => '0744',
-    owner   => 'www-data',
-    group   => 'www-data',
-    content => 'I love Puppet',
+  ensure  => file,
+  path    => '/tmp/school',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet'
 }
